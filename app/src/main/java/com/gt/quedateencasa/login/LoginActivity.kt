30b4958157290d20059c1c43bb994caa82590287
SolisNestor <nestor1186@gmail.com>
2020-03-23 11:49:38 -0600

    added login activity

---
+++ app/src/main/java/com/gt/quedateencasa/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.gt.quedateencasa.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gt.quedateencasa.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
+
