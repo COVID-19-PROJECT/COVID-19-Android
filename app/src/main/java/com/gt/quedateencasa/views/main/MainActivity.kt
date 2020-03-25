eaff0b6f97ceec395065641885eb11d354aa9391
Nelson Saloj <nisaloj@live.com>
2020-03-24 23:05:13 -0600

    Structure navigator, and home screen

--- app/src/main/java/com/gt/quedateencasa/views/main/MainActivity.kt
+++ app/src/main/java/com/gt/quedateencasa/views/main/MainActivity.kt
@@ -1,32 +1,94 @@
 package com.gt.quedateencasa.views.main
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.IntegerRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gt.quedateencasa.R
+import com.gt.quedateencasa.views.main.ui.dashboard.DashboardFragment
+import com.gt.quedateencasa.views.main.ui.home.HomeFragment
+import com.gt.quedateencasa.views.main.ui.notifications.NotificationsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.app_title)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        setupNavigation()
+    }
+
+    fun setupNavigation(){
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_emergency, R.id.navigation_help
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        nav_view.setupWithNavController(navController)
+        nav_view.setOnNavigationItemSelectedListener { item -> onNavigationItemSelected(item) }
+    }
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.navigation_home -> {
+                setFragmentSelected(
+                    R.id.navigation_home
+                )
+                true
+            }
+            R.id.navigation_emergency -> {
+                setFragmentSelected(
+                    R.id.navigation_emergency
+                )
+                true
+            }
+            R.id.navigation_help -> {
+                setFragmentSelected(
+                    R.id.navigation_help
+                )
+                true
+            }
+            else -> false
+        }
+        //drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+    fun setFragmentSelected(fragment:Int) {
+        navController.navigate(fragment)
+        /*supportActionBar!!.title = title
+        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, title)
+            .addToBackStack(null).commit()*/
+    }
 
-        this.supportActionBar?.setIcon(R.drawable.app_title);
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id: Int = item.getItemId()
+        return if (id == R.id.action_person) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/gt/quedateencasa/views/main/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gt/quedateencasa/views/main/ui/home/HomeFragment.kt
@@ -1,20 +1,25 @@
 package com.gt.quedateencasa.views.main.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.gt.quedateencasa.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.item_stat.view.*
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,10 +28,67 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        //val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            //textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initRefreshListener()
+        loadData()
+    }
+
+    fun initRefreshListener()
+    {
+        refresh_home.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
+            override fun onRefresh() {
+                loadData()
+            }
+        })
+    }
+
+    fun loadData()
+    {
+        configureStats()
+        loadWebview()
+        refresh_home.setRefreshing(false)
+    }
+    fun configureStats(){
+        val confirmedStat = stat_confirmed;
+        confirmedStat.stat_title.setText("Confirmados")
+        confirmedStat.stat_qty.setText("20")
+        confirmedStat.stat_qty.setTextColor(Color.RED)
+        val confirmedActive = stat_active;
+        confirmedActive.stat_title.setText("Activos")
+        confirmedActive.stat_qty.setText("20")
+        confirmedActive.stat_qty.setTextColor(Color.GREEN)
+        val confirmedRecovered = stat_recovered;
+        confirmedRecovered.stat_title.setText("Recuperados")
+        confirmedRecovered.stat_qty.setText("20")
+        confirmedRecovered.stat_qty.setTextColor(Color.rgb(255, 185, 0))
+        val confirmedDead = stat_dead;
+        confirmedDead.stat_title.setText("Fallecidos")
+        confirmedDead.stat_qty.setText("20")
+    }
+    fun loadWebview(){
+        val webViewUsage = webview_usage
+        webViewUsage.loadUrl("http://www.puedosalirdecasa.es")
+        manageLoading(webViewUsage, progress_bar_usage)
+        val webViewTips = webview_tips
+        webViewTips.loadUrl("http://www.puedosalirdecasa.es")
+        manageLoading(webViewTips, progress_bar_tips)
+    }
+
+    fun manageLoading(webView: WebView, progressBar:ProgressBar)
+    {
+        webView.setWebChromeClient(object : WebChromeClient() {
+        override fun onProgressChanged(view: WebView, progress: Int) {
+            if (progress == 100) {
+                progressBar.visibility = View.GONE;
+            }
+        }
+    })
     }
 }
