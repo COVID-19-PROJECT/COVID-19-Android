91a5e0626c7fb1ef3aa5af14c732217ef230de30
SolisNestor <nestor1186@gmail.com>
2020-03-25 13:48:30 -0600

    added assets

--- app/src/main/java/com/gt/quedateencasa/views/main/ui/emergencyNumbers/EmergencyNumbersViewModel.kt
+++ app/src/main/java/com/gt/quedateencasa/views/main/ui/emergencyNumbers/EmergencyNumbersViewModel.kt
@@ -1,19 +1,42 @@
 package com.gt.quedateencasa.views.main.ui.emergencyNumbers
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.gt.quedateencasa.R
 import com.gt.quedateencasa.views.main.models.EmergencyNumberObject
 
-class EmergencyNumbersViewModel : ViewModel() {
+class EmergencyNumbersViewModel(application: Application) : AndroidViewModel(application) {
 
     private val emergencyNumbers =
         arrayListOf(
-            EmergencyNumberObject("prueba", "prueba", R.drawable.app_title),
-            EmergencyNumberObject("prueba", "prueba", R.drawable.app_title),
-            EmergencyNumberObject("prueba", "prueba", R.drawable.app_title),
-            EmergencyNumberObject("prueba", "prueba", R.drawable.app_title)
+            EmergencyNumberObject(
+                application.getString(R.string.title_emergency_number_gt),
+                application.getString(R.string.emergency_number_gt),
+                R.drawable.ic_gobierno_gt
+            ),
+            EmergencyNumberObject(
+                application.getString(R.string.title_emergency_number_gt),
+                application.getString(R.string.emergency_number_gt_2),
+                R.drawable.ic_gobierno_gt
+            ),
+            EmergencyNumberObject(
+                application.getString(R.string.title_emergency_number_cbm_gt),
+                application.getString(R.string.emergency_number_cbm_gt),
+                R.drawable.ic_cbm_gt
+            ),
+            EmergencyNumberObject(
+                application.getString(R.string.title_emergency_number_cvb_gt),
+                application.getString(R.string.emergency_number_cvb_gt),
+                R.drawable.ic_cvb_gt
+            ),
+            EmergencyNumberObject(
+                application.getString(R.string.title_emergency_number_pnc_gt),
+                application.getString(R.string.emergency_number_pnc_gt),
+                R.drawable.ic_pnc_gt
+            )
         )
 
     fun getNumbers(): LiveData<List<EmergencyNumberObject>> {
