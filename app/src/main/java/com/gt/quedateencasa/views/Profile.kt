2e4ca7807e9614479c7e518cdfc9b36a1216e8f2
Nelson Saloj <nisaloj@live.com>
2020-03-26 21:57:41 -0600

    Profile screen

---
+++ app/src/main/java/com/gt/quedateencasa/views/Profile.kt
@@ -0,0 +1,13 @@
+package com.gt.quedateencasa.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gt.quedateencasa.R
+
+class Profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
